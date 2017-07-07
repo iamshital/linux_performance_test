d9aa3cb8d9b06e22fc01aafe08dccb9c4b57a5bb
Shital Savekar <iamshital@users.noreply.github.com>
2017-07-07 20:38:51 +0530

    Updated according to Azure Automation.

--- run_ntttcp-for-linux/run-ntttcp-and-tcping.sh
+++ run_ntttcp-for-linux/run-ntttcp-and-tcping.sh
@@ -3,10 +3,10 @@
 log_folder=$1
 server_ip=$2
 server_username=$3
-test_run_duration=60
+test_run_duration=$4
 test_threads_collection=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 6144 8192 10240)
 max_server_threads=64
-eth_name=bond0
+eth_name=$5
 
 if [[ $log_folder == ""  ]]
 then
