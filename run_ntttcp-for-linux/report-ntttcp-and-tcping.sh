23611639cd6f8eb0894fce12727184c2b25860bb
Shital Savekar <iamshital@users.noreply.github.com>
2018-01-29 18:47:05 -0800

    Fixed issues with custom test connections

--- run_ntttcp-for-linux/report-ntttcp-and-tcping.sh
+++ run_ntttcp-for-linux/report-ntttcp-and-tcping.sh
@@ -3,15 +3,15 @@
 log_folder=$1
 ntttcp_log_prefix="ntttcp-sender-p"
 lagscope_log_prefix="lagscope-ntttcp-p"
-test_threads_collection=$2
+test_threads_collection="$2"
 max_server_threads=64
 result_parsed=""
 result_file="${log_folder}/report.log"
 
 if [[ $log_folder == ""  ]]
 then
-	echo "please specify a log folder"
-	exit
+        echo "please specify a log folder"
+        exit
 fi
 
 function get_throughput(){
@@ -36,13 +36,13 @@ function get_throughput(){
 }
 
 function get_cyclesperbyte(){
-	while read line
-	do
-		if [[ "$line" == *"cycles/byte"* ]]
-		then
-			echo "$line" | awk -F'\t:' '{print $2}' 
-		fi
-	done < $1
+        while read line
+        do
+                if [[ "$line" == *"cycles/byte"* ]]
+                then
+                        echo "$line" | awk -F'\t:' '{print $2}'
+                fi
+        done < $1
 }
 
 function get_latency(){
@@ -51,7 +51,7 @@ function get_latency(){
         do
                 if [[ "$line" == *"Average"* ]]
                 then
-                        temp2=`echo "$line" | awk -F'=' '{print $4}' | awk -F'ms' '{print $1}'`
+                        temp2=`echo "$line" | awk -F'=' '{print $4}' | awk -F'us' '{print $1}'`
                         echo $temp2
                         average="NotEmpty"
                 fi
@@ -61,46 +61,46 @@ function get_latency(){
         fi
 }
 
-echo "#test_connections	throughput_gbps	cycles/bytes	average_tcp_latency" > $result_file
+echo "#test_connections throughput_gbps cycles/bytes    average_tcp_latency" > $result_file
 i=0
-while [ "x${test_threads_collection[$i]}" != "x" ]
-do
-	current_test_threads=${test_threads_collection[$i]}
-	if [ $current_test_threads -lt $max_server_threads ]
-	then
-		num_threads_P=$current_test_threads
-		num_threads_n=1
-	else
-		num_threads_P=$max_server_threads
-		num_threads_n=$(($current_test_threads / $num_threads_P))
-	fi
-	
-	ntttcp_log_file="$log_folder/${ntttcp_log_prefix}${num_threads_P}X${num_threads_n}.log"
-	lagscope_log_file="$log_folder/${lagscope_log_prefix}${num_threads_P}X${num_threads_n}.log"
+#while [ "x${test_threads_collection[$i]}" != "x" ]
+#do
+for current_test_threads in $test_threads_collection; do
+        #current_test_threads=${test_threads_collection[$i]}
+        if [ $current_test_threads -lt $max_server_threads ]
+        then
+                num_threads_P=$current_test_threads
+                num_threads_n=1
+        else
+                num_threads_P=$max_server_threads
+                num_threads_n=$(($current_test_threads / $num_threads_P))
+        fi
 
-	throughput=$(get_throughput $ntttcp_log_file)
-	cyclesperbytes=$(get_cyclesperbyte $ntttcp_log_file)
-	latency=$(get_latency $lagscope_log_file)
+        ntttcp_log_file="$log_folder/${ntttcp_log_prefix}${num_threads_P}X${num_threads_n}.log"
+        lagscope_log_file="$log_folder/${lagscope_log_prefix}${num_threads_P}X${num_threads_n}.log"
 
-	if  [ "x$throughput" == "x" ]
-	then
-		throughput=0
-	fi
-	
-	if  [ "x$cyclesperbytes" == "x" ]
-	then
-		cyclesperbytes=0
-	fi
-	
-	if [ "x$latency" == "x" ]
-	then
-		latency=0
-	fi
-	
-	printf "%4s  %8.2f %8.2f %8.2f\n" ${current_test_threads} ${throughput} ${cyclesperbytes} ${latency} >> $result_file
+        throughput=$(get_throughput $ntttcp_log_file)
+        cyclesperbytes=$(get_cyclesperbyte $ntttcp_log_file)
+        latency=$(get_latency $lagscope_log_file)
 
-	i=$(($i + 1))
+        if  [ "x$throughput" == "x" ]
+        then
+                throughput=0
+        fi
+
+        if  [ "x$cyclesperbytes" == "x" ]
+        then
+                cyclesperbytes=0
+        fi
+
+        if [ "x$latency" == "x" ]
+        then
+                latency=0
+        fi
+
+        printf "%4s  %8.2f %8.2f %8.2f\n" ${current_test_threads} ${throughput} ${cyclesperbytes} ${latency} >> $result_file
+
+        i=$(($i + 1))
 done
 
 cat $result_file
-
